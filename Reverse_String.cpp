2014acb4f63f1992238c1a7d3d824d66f6633b18
Priyatosh Nayak <119726190+Priyatosh11@users.noreply.github.com>
2023-10-01 11:19:22 -0700

    Create Reverse_String.cpp

---
+++ Reverse_String.cpp
@@ -0,0 +1,18 @@
+example
+Input: s = "Let's take LeetCode contest"
+Output: "s'teL ekat edoCteeL tsetnoc"
+
+  class Solution {
+public:
+    int climbStairs(int n) {
+         int prev2 = 1;
+  int prev = 1;
+  
+  for(int i=2; i<=n; i++){
+      int cur_i = prev2+ prev;
+      prev2 = prev;
+      prev= cur_i;
+  }
+  return prev;
+    }
+};
